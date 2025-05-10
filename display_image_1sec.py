ae3d5e6f1acd63cdc4578e2347e3a378270b5037
cat2151 <fct_2004@mail.goo.ne.jp>
2025-05-10 15:21:06 +0900

    常駐するようにした。tomlで指定した名前付きパイプで、messageを受け取るようにした。messageを受け取ったら1秒最前面化し、のち最背面化するようにした。これにより、フォーカスを奪わずに最前面に画像を表示できるようにした。

--- display_image_1sec.py
+++ display_image_1sec.py
@@ -1,5 +1,9 @@
+from threading import Thread
 import tkinter
 import toml
+import win32pipe
+import win32file
+import pywintypes
 
 def main():
     args = get_args()
@@ -36,10 +40,11 @@ def display_image(args):
     (root, canvas) = create_gui(x, y, args.pos_x, args.pos_y, f"display-image-1sec : {args.png_filename}")
     load_image_to_canvas(x, y, args.png_filename, root, canvas)
     print_string_to_canvas(x, y, args.disp_string, args.font, args.font_size, root, canvas)
+    do_backmost(root)
 
-    # 1秒待機
-    root.after(args.disp_msec)
-    root.destroy()
+    server_thread = Thread(target=run_ipc_listener, args=(args.pipe_name, root, args.disp_msec), daemon=True)
+    server_thread.start()
+
     root.mainloop()
 
 def create_gui(x, y, pos_x, pos_y, title):
@@ -71,6 +76,56 @@ def print_string_to_canvas(x, y, disp_string, font, font_size, root, canvas):
     canvas.create_text(x / 2 - 1, y / 2 + 1, text=disp_string, font=(font, font_size), fill="black")
     # 本文
     canvas.create_text(x / 2, y / 2, text=disp_string, font=(font, font_size), fill="white")
+    root.update()
+
+def run_ipc_listener(pipe_name, root, disp_msec):
+    while True:
+        pipe = create_named_pipe(pipe_name)
+        try:
+            print("Waiting for client connection...")
+            win32pipe.ConnectNamedPipe(pipe, None)
+            print("Client connected.")
+            while True:
+                try:
+                    handle_received_message(pipe, root, disp_msec)
+                except pywintypes.error as e:
+                    if e.args[0] == 109:  # ERROR_BROKEN_PIPE
+                        print("Client disconnected.")
+                        break
+                    else:
+                        raise
+        finally:
+            win32file.CloseHandle(pipe)
+
+def create_named_pipe(pipe_name):
+    pipe = win32pipe.CreateNamedPipe(
+        pipe_name,
+        win32pipe.PIPE_ACCESS_DUPLEX,
+        win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE | win32pipe.PIPE_WAIT,
+        1, 65536, 65536,
+        0,
+        None
+    )
+    return pipe
+
+def handle_received_message(pipe, root, disp_msec):
+    resp = win32file.ReadFile(pipe, 64*1024)
+    message = resp[1].decode()
+    print(f"Received: {message}")
+    # ↑ TODO messageに応じた画像の表示をする。そのため、引数に actions を追加する。tomlに [[actions]] を書き、args.actions で受け渡す。action_name = 'disp_png' などを想定。
+
+    do_topmost(root)
+    root.after(disp_msec, do_backmost, root)
+
+    win32file.WriteFile(pipe, b"Message received")
+
+def do_topmost(root):
+    root.attributes("-topmost", True)
+    root.update()
+
+def do_backmost(root):
+    root.attributes("-topmost", False)
+    root.lower()
     root.update()
 
 if __name__ == "__main__":
