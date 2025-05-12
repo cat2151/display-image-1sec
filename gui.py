dc54b78bf4004ecb1efdfe00eb5ae7014bf6099e
cat2151 <fct_2004@mail.goo.ne.jp>
2025-05-12 09:08:01 +0900

    backmost時は ALT+TAB の対象外とするようにした

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+import ctypes
 import tkinter
 from PIL import Image, ImageTk
 
@@ -42,7 +43,23 @@ def do_topmost(root):
     root.attributes("-topmost", True)
     root.update()
 
+    # ALT + TAB の対象にする
+    hwnd = ctypes.windll.user32.GetParent(root.winfo_id())
+    GWL_EXSTYLE = -20
+    WS_EX_TOOLWINDOW = 0x00000080
+    current_style = ctypes.windll.user32.GetWindowLongW(hwnd, GWL_EXSTYLE)
+    ctypes.windll.user32.SetWindowLongW(hwnd, GWL_EXSTYLE, current_style & ~WS_EX_TOOLWINDOW)
+    root.update()
+
 def do_backmost(root):
     root.attributes("-topmost", False)
     root.lower()
+    root.update()
+
+    # ALT + TAB の対象外にする
+    hwnd = ctypes.windll.user32.GetParent(root.winfo_id())
+    GWL_EXSTYLE = -20
+    WS_EX_TOOLWINDOW = 0x00000080
+    current_style = ctypes.windll.user32.GetWindowLongW(hwnd, GWL_EXSTYLE)
+    ctypes.windll.user32.SetWindowLongW(hwnd, GWL_EXSTYLE, current_style | WS_EX_TOOLWINDOW)
     root.update()
